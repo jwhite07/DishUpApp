2135e3187d68c5054fa9c67ecab45f056ce3a064
James White <jwhite@eliteworx.com>
2015-08-21 13:14:26 -0400

    Pods stuff

---
+++ Pods/Target Support Files/Pods-DishUp/Pods-DishUp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DishUp : NSObject
+@end
+@implementation PodsDummy_Pods_DishUp
+@end
---
+++ Pods/Target Support Files/Pods-DishUp/Pods-DishUp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DishUpVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DishUpVersionString[];
+
